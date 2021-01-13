4f9494a34be2efb305a0b6abcbaa4b539ae3a18f
kpandian <kpandian@ucsc.edu>
2021-01-12 18:59:30 -0800

    Sample

---
+++ src/Components/Body/Request/Request.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Request.css';
+
+
+const Request: <React.FC> = () => {
+    return(
+        <React.Fragment>
+            <div class="">
+                
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Request;
