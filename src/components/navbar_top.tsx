6940f1ce967343c400f7c3a9b25d58dd82fd3070
Dmitry Smirnov <dmitrysmi010@gmail.com>
2021-01-14 14:28:24 -0800

    top navbar and its css, work in progress and looks quite ugly

---
+++ src/components/navbar_top.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './navbar_top.css'
+
+class NavbarTop extends Component {
+  render() {
+    return (
+       
+        <div className="topnav">
+            <h1><a>Welocate</a></h1>
+        </div>
+   );
+  }
+
+}
+export default NavbarTop;
