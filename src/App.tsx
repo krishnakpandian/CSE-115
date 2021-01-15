b53ae0644421870cd96248490a309325b848973f
jonahkula <jonahkula@gmail.com>
2021-01-15 15:02:48 -0800

    bulma

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
+import "./bulma.css"
+import NavbarTop from "./components/navbar_top";
 import ResultBody from './components/result-body'
 import Search from './components/search/Search'
 
 function App() {
   return (
     <div className="App">
-      <div>test</div>
+      <NavbarTop />
       <Search />
       <ResultBody />
     </div>
