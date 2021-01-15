a38d110e55d790ea553a81b726e8d663774422f2
Dmitry Smirnov <dmitrysmi010@gmail.com>
2021-01-15 15:44:24 -0800

    Modified navbar, now completely bulma and has login/signup buttons that print to console

--- src/components/navbar_top.tsx
+++ src/components/navbar_top.tsx
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
 import './navbar_top.css'
+import "../bulma.css"
 
 class NavbarTop extends Component {
   render() {
     return (
-       
-        <div className="topnav">
-            <h1><a>Welocate</a></h1>
+      <nav className="navbar is-fixed-top has-background-danger" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <h1 className="navbar-item title is-1">
+            We-Locate
+          </h1>
         </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <button className="button" onClick={this.signUpClick}>Sign Up</button>
+          </div>
+          <div className="navbar-item">
+            <button className="button" onClick={this.logInClick}>Login</button>
+          </div>
+        </div>
+      </nav> 
    );
   }
 
+  logInClick() {
+    console.log("Login button has been pressed.");
+  }
+  signUpClick() {
+    console.log("Sign up button has been pressed.");
+  }
 }
 export default NavbarTop;
