c9f83852575163fa9e4c83bd8b1b802502d4a24b
Dmitry Smirnov <dmitrysmi010@gmail.com>
2021-01-15 16:34:17 -0800

    Add footer, has non-interactive about us/contact links, separated width parameters for top/bot navbars

---
+++ src/components/navbar_bottom.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import './navbar_bottom.css'
+import "../bulma.css"
+
+class NavbarBottom extends Component {
+  render() {
+    return (
+      <nav className="navbar is-fixed-bottom has-background-danger" id="bottom_navbar" role="navigation" aria-label="footer navigation">
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <p>About Us</p>
+          </div>
+          <div className="navbar-item">
+            <p>Contact</p>
+          </div>
+        </div>
+      </nav> 
+   );
+  }
+}
+export default NavbarBottom;
