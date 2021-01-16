f3d15d9003bd05fe1b1b50a0b7034e6ca9d25ab7
Dmitry Smirnov <dmitrysmi010@gmail.com>
2021-01-16 13:57:14 -0800

    Display bottom bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import "./bulma.css"
 import NavbarTop from "./components/navbar_top";
+import NavbarBottom from "./components/navbar_bottom";
 import ResultBody from './components/result-body'
 import Search from './components/search/Search'
 
@@ -9,8 +10,10 @@ function App() {
   return (
     <div className="App">
       <NavbarTop />
+      <div>
       <Search />
-      <ResultBody />
+      </div>
+      <NavbarBottom />
     </div>
   );
 }
