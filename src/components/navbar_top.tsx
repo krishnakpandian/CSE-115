32729dbc98dbaeb47a4270abcedaffc8a121bfe6
Dmitry Smirnov <dmitrysmi010@gmail.com>
2021-01-18 17:34:14 -0800

    Buttons no longer go out of bounds when screen is minimized, instead a menu button appears. Note that it is not yet functional

--- src/components/navbar_top.tsx
+++ src/components/navbar_top.tsx
@@ -5,19 +5,28 @@ import "../bulma.css"
 class NavbarTop extends Component {
   render() {
     return (
-      <nav className="navbar has-background-danger" id="top_navbar"role="navigation" aria-label="main navigation">
+      <nav className="navbar has-background-danger" id="top_navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
+          <div className="navbar-brand">
           <h1 className="navbar-item title is-1">
             We-Locate
           </h1>
-        </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <button className="button" onClick={this.signUpClick}>Sign Up</button>
           </div>
-          <div className="navbar-item">
-            <button className="button" onClick={this.logInClick}>Login</button>
+          <div role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </div>
+        </div>
+        
+        <div id="navbarButtons" className="navbar-menu">
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <button className="button" onClick={this.signUpClick}>Sign Up</button>
+                <button className="button" onClick={this.logInClick}>Login</button>
+              </div>
+            </div>
           </div>
         </div>
       </nav> 
