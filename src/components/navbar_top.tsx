af3e5626af7a81e438a0806557685ed1f5620d93
Dmitry Smirnov <dmitrysmi010@gmail.com>
2021-01-18 18:35:56 -0800

    Login/signup buttons now minimized into a dropdown menu when screen resolution is decreased

--- src/components/navbar_top.tsx
+++ src/components/navbar_top.tsx
@@ -2,20 +2,44 @@ import React, { Component } from "react";
 import './navbar_top.css'
 import "../bulma.css"
 
-class NavbarTop extends Component {
+type state = { collapsed: boolean };
+type props = {};
+class NavbarTop extends React.Component<props,state> {
+
+  constructor(props) {
+    super(props);
+    this.state = { collapsed: true };
+  }
+
+  handleToggle() {
+    this.setState({ collapsed: !this.state.collapsed });
+  }
+
   render() {
     return (
       <nav className="navbar has-background-danger" id="top_navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
           <div className="navbar-brand">
-          <h1 className="navbar-item title is-1">
-            We-Locate
-          </h1>
+            <h1 className="navbar-item title is-1">
+              We-Locate
+            </h1>
           </div>
-          <div role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
+
+          <div className={"navbar-burger dropdown is-right" + (this.state.collapsed ? "" : " is-active")} onClick={() => this.handleToggle()} aria-label="menu"
+            aria-haspopup="true" aria-controls="dropdown-menu" aria-expanded="false">
+            <div className="dropdown-trigger">
+              
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              
+            </div>
+            <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                <div className="dropdown-content">
+                  <button className="button dropdown-item" onClick={this.signUpClick}>Sign Up</button>
+                  <button className="button dropdown-item" onClick={this.logInClick}>Login</button>
+                </div>
+            </div>
           </div>
         </div>
         
