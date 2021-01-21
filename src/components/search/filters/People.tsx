4336e4c0204315f9c4c59608378a8722822c8f60
jonahkula <jonahkula@gmail.com>
2021-01-20 19:50:16 -0800

    live update of filters

--- src/components/search/filters/People.tsx
+++ src/components/search/filters/People.tsx
@@ -3,7 +3,19 @@ import './People.css';
 const People = ({ currentPeop, setCurrentPeop }) => {
   return (
     <div className='People'>
-      people filter
+      <div className="field">
+        <div className="control">
+          <div className="select is-info">
+            <select value={currentPeop} onChange={(e) => setCurrentPeop(e.target.value)}>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5+</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/search/filters/Radius.tsx
+++ src/components/search/filters/Radius.tsx
@@ -3,7 +3,18 @@ import './Radius.css';
 const Radius = ({ currentRad, setCurrentRad }) => {
   return (
     <div className='Radius'>
-      radius filter
+      <div className="field">
+        <div className="control">
+          <div className="select is-info">
+            <select value={currentRad} onChange={(e) => setCurrentRad(e.target.value)}>
+              <option>25 miles</option>
+              <option>50 miles</option>
+              <option>75 miles</option>
+              <option>100+ miles</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
