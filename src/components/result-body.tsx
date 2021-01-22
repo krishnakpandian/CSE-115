ae149630903176e6b39c58b899badb8ccfd3c4fa
unknown <wli108@ucsc.edu>
2021-01-21 19:27:33 -0800

    Added very barebones cards

--- src/components/result-body.tsx
+++ src/components/result-body.tsx
@@ -1,108 +1,52 @@
 import React, { Component } from "react";
+import { checkServerIdentity } from "tls";
 import dog from '../assets/dog2.jpg'
 import "../bulma.css"
 import "./result-body.css"
 
-// ResultBody Contains the 6 rectangles that will host the results of a search
-// Also contains the back and next buttons
-// Back and next button call backClick() and nextClick() respectively
-// Clicking on an image calls imgClick()
-// Formatted using bulma columns, split into three columns
-// Each column has two images on them with automatically stacked on the other
-// The first and third column have the back and next button on the bottom respectively
-
-// Columns currently in use but tiles might be better later when bringing in data
-
-class resultBody extends Component {
-  render() {
-    return (
-      <section>
-
-        <div className="columns">
-
-          <div className="column">
-            <figure className="image">
-              <img src={dog} onClick={this.imgClick} />
-            </figure>
-            <figure className="image">
-              <img src={dog} onClick={this.imgClick} />
-            </figure>
-            <button className="button" onClick={this.backClick}>Back</button>
-          </div>
-
-          <div className="column">
-            <figure className="image">
-              <img src={dog} onClick={this.imgClick} />
-            </figure>
-            <figure className="image">
-              <img src={dog} onClick={this.imgClick} />
-            </figure>
-          </div>
-
-          <div className="column">
-            <figure className="image">
-              <img src={dog} onClick={this.imgClick} />
-            </figure>
-            <figure className="image">
-              <img src={dog} onClick={this.imgClick} />
-            </figure>
-            <button className="button" onClick={this.nextClick}>Next</button>
-          </div>
-
-        </div>
-
-      </section >
-    );
-  }
-
-  backClick() {
-    console.log("Back button has been pressed.");
-  }
-
-  nextClick() {
-    console.log("Next button has been pressed.");
-  }
-
-  imgClick() {
-    console.log("You clicked on this image.")
-  }
-
-}
-
-
-
-interface results{
+interface results {
   cityName: string,
   distance?: number,
   travelTime?: number,
   averageCost?: number
 }
 
-interface props{
+interface props {
   results: results[],
   statusCode: number,
   message: string
 }
 
-const ResultBody: React.FC<props> = ( {results, statusCode, message} ) => {
-    console.log(results);
-    return(
-      <>
-        <div className="result-container">
-            component Renders
-            { results.map( (result) => {
-                return(
-                  <>
-                    <div>
-                        Hello
-                    </div>
-                  </>
-                )
-            })}
-        </div>
-      </>
-    )
+const ResultBody: React.FC<props> = ({ results, statusCode, message }) => {
+  console.log(results);
+  return (
+    <>
+      <div className="result-container">
+        {results.map((result) => {
+          return (
+            <div className="card">
+              <div className="title">
+                {result.cityName}
+              </div>
+              <div className="card-content distance">
+                {result.distance} Miles
+                </div>
+              <div className="card-content averageCost">
+                ${result.averageCost}
+              </div>
+              <div className="card-content travelTime">
+                {result.travelTime} Minutes
+              </div>
+              <div className="card-footer">
+                <p className="card-footer-item">
+                  View
+                </p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
 }
-
-
 export default ResultBody;
