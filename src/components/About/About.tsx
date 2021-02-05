646be068b912b62ef48ec3746f3358090e4f90ff
kpandian <kpandian@ucsc.edu>
2021-02-05 12:49:34 -0800

    Fixing about page, adding npm module, changing modularity

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {developers} from './Props/developers';
 import './About.css';
 import DeveloperCard from './DeveloperCard/DeveloperCard';
+import Fade from 'react-reveal/Fade';
 
 const About: React.FC = () => {
     return (
       <>
         <div className="about-container">
           <div className= "title"> About Us</div>
+          <Fade down delay={500} distance={"0.5em"}>
           <div className="developer-list">
           {developers.map((developer, i) => {
                 return(
@@ -17,6 +19,7 @@ const About: React.FC = () => {
             )    
           })}
           </div>
+          </Fade>
         </div>
       </>
     )
--- src/components/NavBar/navbar_top.tsx
+++ src/components/NavBar/navbar_top.tsx
@@ -3,9 +3,6 @@ import { useState } from 'react';
 import PropTypes from "prop-types";
 import './navbar_top.css'
 import "../../bulma.css"
-import truck_logo from '../../assets/moving_truck.png'
-import form_logo from '../../assets/form_icon.png'
-import apartment_logo from '../../assets/apartment_icon.png'
 import newUser from '../Signup/signup_form'
 import loginUser from '../Login/login_form'
 import firebase from 'firebase';
@@ -133,43 +130,6 @@ class NavbarTop extends React.Component<any, any> {
       </nav> 
       { this.state.signupCollapsed ? <this.Signup /> : null }
       { this.state.loginCollapsed ? <this.Login /> : null }
-      <div className="columns is-centered" id="about_section">
-        <div className="column is-one-third">
-          <div id="info_card">
-            <div className='is-flex is-horizontal-center'>
-              <figure className='image is-64x64'><img src={form_logo}></img></figure>
-            </div>
-            <h1 className="title is-2"> Search </h1>
-            <p className="is-size-4">Enter an address to receive vital information
-              about cost of living, traffic, and distances to 
-              places to visit.
-            </p>
-            </div>
-        </div>
-        <div className="column is-one-third">
-          <div id="info_card">
-            <div className='is-flex is-horizontal-center'>
-              <figure className='image is-64x64'><img src={apartment_logo}></img></figure>
-            </div>
-            <h1 className="title is-2"> Choose </h1>
-            <p className="is-size-4">Pick a place that fits your creteria from the results
-              and save it along with other relevant links. 
-            </p>
-          </div>
-        </div>
-        <div className="column is-one-third">
-          <div id="info_card">
-              <div className='is-flex is-horizontal-center'>
-                <figure className='image is-64x64'><img src={truck_logo}></img></figure>
-              </div>
-              <h1 className="title is-2"> Relocate </h1>
-              <p className="is-size-4">Now that you are informed and prepared, it is time to make your 
-                life-changing move. 
-              </p>
-              
-            </div>
-          </div>
-      </div>
       </section>
    );
   }
