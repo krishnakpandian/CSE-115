e6d2062e8702d6f8cea7233906c23d2284951c3f
Dmitry Smirnov <dmitrysmi010@gmail.com>
2021-02-12 16:11:12 -0800

    Basic google signup function, no functionality so far

---
+++ src/components/Signup/google_signup.tsx
@@ -0,0 +1,32 @@
+import firebase from 'firebase';
+import {firebaseConfig} from './firebaseConfig'
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+
+firebase.auth().signInWithRedirect(provider);
+
+firebase.auth()
+  .getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential as firebase.auth.OAuthCredential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
