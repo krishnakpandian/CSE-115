7f0a3d90b9087b7b5a8c6d301ce609b02f24c09d
Dmitry Smirnov <dmitrysmi010@gmail.com>
2021-02-16 17:48:56 -0800

    Fixed database info so saved cards now added properly for google accounts

--- src/components/Signup/google_signup.tsx
+++ src/components/Signup/google_signup.tsx
@@ -7,32 +7,35 @@ const db = firebase.firestore();
 
 async function newGoogleUser() {
   let uid: string | undefined;
-  firebase.auth().signInWithRedirect(provider);
+
 
   firebase.auth()
-    .getRedirectResult()
-    .then((result) => {
-      if (result.credential) {
-        /** @type {firebase.auth.OAuthCredential} */
-        const credential = result.credential as firebase.auth.OAuthCredential;
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    const credential = result.credential as firebase.auth.OAuthCredential;
 
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const token = credential.accessToken;
-      }
-      // The signed-in user info.
-      const user = result.user;
-      uid = result.user?.uid;
-      db.collection("Users").doc(uid).set({
-        name: "Google User!"
-      })
-    }).catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      
-      const credential = error.credential;
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    uid = result.user?.uid;
+    db.collection("Users").doc(uid).set({
+      name: "Google User!"
+    }).then(function() {
+      window.alert("Doc completion notification.")
     });
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    const credential = error.credential;
+    // ...
+  });
 }
 
 export default newGoogleUser;
