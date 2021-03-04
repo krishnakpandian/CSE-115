a2dc9a836233da28302778dd102bbbc0604dfa00
kpandian <kpandian@ucsc.edu>
2021-03-03 17:24:01 -0800

    Updated tests

--- src/components/Signup/google_signup.tsx
+++ src/components/Signup/google_signup.tsx
@@ -1,7 +1,5 @@
 import firebase from './firebaseConfig';
 
-//get google auth provider
-const provider = new firebase.auth.GoogleAuthProvider();
 
 //get firebase database 
 const db = firebase.firestore(); 
@@ -10,9 +8,9 @@ const db = firebase.firestore();
    Has no arguments and returns nothing
    Creates a database entry for that user
 */
-async function newGoogleUser() {
+export async function newGoogleUser() {
   let uid: string | undefined;
-
+  const provider = new firebase.auth.GoogleAuthProvider();
 
   firebase.auth()
   .signInWithPopup(provider)
